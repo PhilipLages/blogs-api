6852a79fd0fc7ba84c424d0f33d748a3f4f5df7b
Philip Lages <psilvaelages@gmail.com>
2022-11-30 19:06:34 -0300

    refactor: adds name to category model

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  const category = sequelize.define({
+  const category = sequelize.define('Category', {
+    id: DataTypes.NUMBER,
     name: DataTypes.STRING
   }, {
     sequelize,
