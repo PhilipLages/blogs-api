fca2fc1b2faa75574c700d5c35270fcfea00277c
Philip Lages <psilvaelages@gmail.com>
2022-12-01 17:03:25 -0300

    feat: adds authmiddleware

--- src/middlewares/user.middlewares.js
+++ src/middlewares/user.middlewares.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const loginBody = require('./joi');
 
 const validateLogin = (req, res, next) => {
@@ -12,8 +13,27 @@ const validateLogin = (req, res, next) => {
   return next();
 };
 
-const authMiddleware = () => {
+const authMiddleware = (req, res, next) => {
+  const { authorization } = req.headers;
 
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  const token = authorization.replace('Bearer', '').trim();
+  const secret = process.env.JWT_SECRET || 'yourSecretKey';
+
+  try {
+    const data = jwt.verify(token, secret);
+
+    const { id } = data;
+
+    req.userId = id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
 };
 
 module.exports = {
